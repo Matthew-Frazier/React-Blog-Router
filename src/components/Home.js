ab8129b152bc4e57671a12961e4d19ae5773258d
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-06-06 13:56:36 -0600

    router set up with edit and notes

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Segment, } from 'semantic-ui-react';
+
+const Home = () => (
+  <Segment style={{ backgroundColor: "grey", height: "50px", margin: "0", padding: "0" }}>
+    <h1 style={{ color: "blue", textAlign: "center"}}>Home</h1>
+  </Segment>
+);
+
+export default Home;
